a16563b0cfba77c5c1bbcd2c9525329950652873
factvlad <fact1@ukr.net>
2022-08-15 23:17:32 +0300

    1st task

--- src/components/Feedback/Notification.jsx
+++ src/components/Feedback/Notification.jsx
@@ -1,7 +1,9 @@
 import PropTypes from "prop-types"
+import Notiflix from 'notiflix';
 import style from "./Feedback.module.scss"
 
 const Notification = ({ message }) => {
+  Notiflix.Report.info('There is no feedback', '', 'Click ');
   return (<>
     <p className={ style.noti } >{ message }</p>
   </>
