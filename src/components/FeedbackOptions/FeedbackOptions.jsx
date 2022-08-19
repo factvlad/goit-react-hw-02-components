118d8bffe5bd8c0e884110dc31eb2b01f139fd04
factvlad <fact1@ukr.net>
2022-08-19 09:46:23 +0300

    fix fix fix

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,24 +2,21 @@ import PropTypes from "prop-types"
 import style from "../Feedback.module.scss"
 
 const FeedbackOptions = ({ leaveVote, options }) => {
+
   return (
-    <div className={ style.btnDiv }>
-      { options.map((option, index) => {
-        const label =
-          option.split('')[0].toUpperCase() +
-          option.split('').slice(1).join('');
-        return (
+    <ul className={ style.btnDiv }>
+      { options.map((option, index) => (
+        <li key={ index }>
           <button
             className={ style.btn }
-            key={ index }
             onClick={ () => leaveVote(option) }
             type="button">
-            { label }
+            { option.split('')[0].toUpperCase() +
+              option.split('').slice(1).join('') }
           </button>
-        );
-      }) }
-    </div>
-
+        </li>
+      )) }
+    </ul>
   );
 }
 export default FeedbackOptions;
